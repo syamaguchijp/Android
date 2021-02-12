daa0867fda1935f7c441b0a35e437db24eab44ee
syamaguchijp <bergjapan@gmail.com>
2021-02-12 20:40:16 +0900

    mod Date

--- Logic/Others/Snippets/Date.kt
+++ Logic/Others/Snippets/Date.kt
@@ -1,6 +1,8 @@
 import java.util.Date
 import java.util.GregorianCalendar
 import java.util.Calendar
+import java.util.TimeZone
+import java.util.Locale
 import java.text.SimpleDateFormat
 
 class DateUtil() {
@@ -31,9 +33,18 @@ class DateUtil() {
          * 日付の加減計算
          */
         fun getDate(date: Date, dateDelta: Int): Date {
-            val cal = Calendar.getInstance()
+            val cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
             cal.time = date
             cal.add(Calendar.DATE, dateDelta) // dateDelta日後（マイナスの場合はdateDelta日前）
+            return cal.time
+        }
+
+        /**
+         * 年月日時分秒からDateオブジェクトを生成
+         */
+        fun getDate(year: Int, month: Int, date: Int, hour: Int, minute: Int, second: Int): Date? {
+            val cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"), Locale.JAPAN)
+            cal.set(year, month-1, date, hour, minute, second) // monthは0から11の値を入力する
             return cal.time
         }
     }
@@ -44,4 +55,13 @@ fun main() {
     println(DateUtil.covertToString(Date(), DateUtil.gregorianFormatterISO8601()))
     println(DateUtil.covertToDate("2021-02-12T11:29:54+0900", DateUtil.gregorianFormatterISO8601()))  
     println(DateUtil.getDate(Date(), 7))
+    println(DateUtil.getDate(2021, 2, 15, 1, 2, 33))
+
+    // ２つの日時の差分（秒）
+    val cal1 = Calendar.getInstance()
+    cal1.set(2021, 2, 15, 1, 2, 33)
+    val cal2 = Calendar.getInstance()
+    cal2.set(2021, 2, 15, 1, 2, 34)
+    println(((cal2.timeInMillis - cal1.timeInMillis) / 1000).toInt()) // ミリセカンドを秒に変換
+
 }
