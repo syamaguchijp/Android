43adba07650e948c614d30ac40bdf51d08ed52e7
Yamaguchi <yamaguchi@YamaguchinoMacBook-Air.local>
2021-08-07 09:51:22 +0900

    add Drawer

---
+++ UI/NavigationDrawer/app/src/main/java/com/example/sample/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,48 @@
+package com.example.sample.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.sample.Logging
+import com.example.sample.R
+import com.example.sample.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Logging.d("")
+        galleryViewModel =
+            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        Logging.d("")
+        super.onDestroyView()
+        _binding = null
+    }
+}
